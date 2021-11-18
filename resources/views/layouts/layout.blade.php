c778365d4f1bd61b15e41dc354a94cc07beb139b
Mickle <lakramorozov@mail.ru>
2021-11-18 17:53:31 +0700

    laravel-mix

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MySite</title>
+	
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+</head>
+<body>
+
+Content
+
+<script src="{{ asset('js/scripts.js') }}"></script>
+
+</body>
+</html>
+
